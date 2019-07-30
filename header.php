dcbd733265a0681480577e6fcc35dd040b7185ea
Muthukumar Karpuram <kumar@lastudios.com.my>
2019-07-30 13:31:25 +0530

    header file

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav id="navigation">
+    <ul id="nav" class="nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Skills</a></li>
+        <li><a href="#">Projects</a></li>
+    </ul>
+</nav>
