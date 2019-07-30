691d513a11074789a1046d3e5fd87bd396038938
Muthukumar Karpuram <kumar@lastudios.com.my>
2019-07-30 13:15:04 +0530

    My resume

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Muthukumar</title>
+
+        <!--JavaScript-->
+        <script src="javascript/javascript.js" type="text/javascript"></script>
+
+        <!--Stylesheet-->
+        <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css" />
+        <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" />
+
+
+        <!--Font Awesome-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
+    </head>
+            
+
+            <header>
+                <?php include 'header.php'; ?>
+            </header> 
+
+    <body>
+        <div class="content_area">
+            <div class="container">            
+                <?php include 'content.php'; ?>
+            </div>
+        </div>
+
+        <div class="content_area">
+            <div class="container">            
+                <?php include 'resume.php'; ?>
+            </div>
+        </div>
+
+        <div class="content_area">
+            <div class="container">            
+                <?php include 'skills.php'; ?>
+            </div>
+        </div>
+
+
+        <div class="content_area">
+            <div class="container">            
+                <?php include 'just.php'; ?>
+            </div>
+        </div>
+
+        <?php include 'footer.php'; ?>
+
+
+    </body>
+</html>
