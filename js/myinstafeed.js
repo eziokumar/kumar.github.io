cf628a6da7091e5f9a936d2c407abaf5bd02eced
Muthukumar Karpuram <kumar@lastudios.com.my>
2021-01-18 13:32:13 +0530

    new instafeed

--- js/myinstafeed.js
+++ js/myinstafeed.js
@@ -1,8 +1,4 @@
 <script type="text/javascript">
-    var userFeed = new Instafeed({
-        get: 'user',
-        userId: 'x3ziox',
-        accessToken: '19826354525.1677ed0.b4cc45acdf2942edbbc0c41a2e3c55f9'
-    });
+   
     userFeed.run();
 </script>
